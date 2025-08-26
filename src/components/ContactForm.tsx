f5db52aef542501769c9b64e2e31b6ad3873a04f
Alexandre Feio <alefeio@gmail.com>
2025-08-26 15:15:43 -0300

    ajustes de informacoes - 4

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -70,7 +70,7 @@ const ContactForm: React.FC = () => {
   };
 
   return (
-    <section className="bg-gray-800 py-12 md:py-20 text-white">
+    <section id='contato' className="bg-gray-800 py-12 md:py-20 text-white">
       <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center">
           <p className="text-orange-400 font-semibold text-lg">Fale Conosco</p>
