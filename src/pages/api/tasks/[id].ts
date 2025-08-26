946d37f4dd4878d327d544e32396c5d267ace48d
Alexandre Feio <alefeio@gmail.com>
2025-08-26 17:37:52 -0300

    gestao de tasks - 2

--- src/pages/api/tasks/[id].ts
+++ src/pages/api/tasks/[id].ts
@@ -1,6 +1,6 @@
 // pages/api/tasks/[id].ts
 import type { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '@/lib/prisma';
+import prisma from "../../../../lib/prisma";
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
   const taskId = req.query.id as string;
